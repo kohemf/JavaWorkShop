61dc522154b8c9015a2c3167c85e3c168f1f05cc
LJH <LJH@112.187.211.165>
2021-05-21 12:46:52 +0900

    java practice

---
+++ MyJavaWorkShop14/src/com/test/MysqlCon.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.sql.*;  
+public class MysqlCon{  
+	public static void main(String args[]){  
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+					"jdbc:mysql://localhost:3306/testdb","test","1234");  
+			Statement stmt=con.createStatement();  
+			ResultSet rs=stmt.executeQuery("SELECT emp_no,first_name");  
+			while(rs.next())  
+				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			con.close();  
+		}catch(Exception e){ System.out.println(e);}  
+	}  
+}
